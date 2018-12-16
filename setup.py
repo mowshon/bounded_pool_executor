db768a2955f657422c52b9505b12fbec0fd284b8
mowshon <mowshon@yahoo.com>
2018-12-16 21:55:54 +0200

    Remove manifest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bounded_pool_executor',
-    version='0.0.1',
+    version='0.0.2',
     description='Bounded Process&Thread Pool Executor',
     long_description=long_description,
     long_description_content_type="text/markdown",
