ce3dfefeb6e4d1b7bb6df9d76f9d60c101a4d475
mowshon <mowshon@yahoo.com>
2018-12-16 21:09:29 +0200

    add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='bounded_pool_executor',
     version='0.0.1',
     description='Bounded Process&Thread Pool Executor',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='http://github.com/mowshon/bounded_pool_executor',
     keywords='concurrent futures ProcessPoolExecutor ThreadPoolExecutor Semaphore memory leak',
     author='Mowshon',
