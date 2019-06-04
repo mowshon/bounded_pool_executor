24b5a36ec4997c23ef377559ace6b3599af0f018
mowshon <vasile.buldumac@ati.utm.md>
2019-06-04 22:31:48 +0300

    version: 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bounded_pool_executor',
-    version='0.0.2',
+    version='0.0.3',
     description='Bounded Process&Thread Pool Executor',
     long_description=long_description,
     long_description_content_type="text/markdown",
